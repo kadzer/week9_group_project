fa34d71d2b70b3b9ea6b44174659e363204d454d
tannyque <tannyque1992@gmail.com>
2018-09-14 19:47:53 +0100

    Adds remove marker

--- client/src/views/map_wrapper.js
+++ client/src/views/map_wrapper.js
@@ -4,17 +4,21 @@ const MapWrapper = function (container) {
   this.container = container;
   this.map = L.map(this.container)
   this.osmLayer = new L.TileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png");
+  this.markers = []
 };
 
 MapWrapper.prototype.bindEvents = function () {
   PubSub.subscribe('Dinosaurs:found-dino', (event) => {
+    this.markers.forEach((marker)=>{
+      this.map.removeLayer(marker);
+    });
     const dinosaur = event.detail;
     const dinoCoordinates = dinosaur.latlng;
-    console.log(dinoCoordinates);
     this.map.setView(dinoCoordinates, 5).addLayer(this.osmLayer);
     const marker = L.marker(dinoCoordinates)
+    this.markers.push(marker)
     marker.addTo(this.map);
-  })
+  });
 };
 
 module.exports = MapWrapper;
